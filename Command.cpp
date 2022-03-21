f455ad58d6e7dc80a413994afa8748a45eddc2f5
JevantJedidia <73844363+JevantJedidia@users.noreply.github.com>
2022-03-21 21:23:04 +0700

    Update Command.cpp

--- Command.cpp
+++ Command.cpp
@@ -1,11 +1,25 @@
 #include "Command.hpp"
 
-//Show
-Show::Show(){
-}
+bool checkID(string InvID){ //validasi ID
+    string ID = InvID;
+    int index;
+    InvID.erase(0,1);
+    index = stoi(InvID);
+    if (ID[0] != 'I'){
+        return false;
+    }
+    if (index < 0 || index > 26){
+        return false;
+    }
+    return true;
+};
 
-void Show::Execute(){
-    cout << "Show" << endl;
+int getSlotID(string InvID){ //ambil ID
+    string ID;
+    int index;
+    ID = InvID.erase(0,1);
+    index = stoi(ID);
+    return index;
 }
 
 //Give
@@ -41,10 +55,16 @@ void Move::Execute(){
 
 //Use
 Use::Use(string InvID){
-    this->InvId = InvID;
+    this->InvID = InvID;
 }
-void Use::Execute(){
-    cout << "Use" << endl;
+void Use::Execute(Table<3,9>* inventory){
+    if (checkID(this->InvID)){
+        inventory->useTool(getSlotID(InvID));
+        cout << "Item used" << endl;
+    }
+    else{
+        cout << "Masukan salah" << endl;
+    }
 }
 
 //Craft
@@ -60,6 +80,39 @@ Export::Export(string fileName){
     this->fileName = fileName;
 }
 
-void Export::Execute(){
-    cout << "Export" << endl;
+bool Export::checkFilename(){
+    regex b("(.*)(.txt)");
+    if (!regex_match(this->fileName,b)){
+        return false;
+    }
+    return true;
+}
+
+void Export::Execute(Table<3,9>* inventory){
+    int row, col, val;
+    string ItemId;
+    if (checkFilename()){
+        ofstream outfile(fileName);
+        for (int i=0;i<27;i++){
+            col = i % 9;
+            row = i / 9;
+            if (inventory->checkEmpty(row,col)){ //cek slot kosong
+                outfile << "0:0" << endl;
+            }
+            else{
+                ItemId = inventory->getItemId(row,col);
+                if (inventory->checkTool(row,col)){
+                    val = inventory->getDur(row,col);
+                }
+                else{
+                    val = inventory->getQty(row,col);
+                }
+                outfile << ItemId << ":" << val << endl;
+            }
+        }
+        outfile.close();
+    }
+    else{
+        cout << "Masukan nama file salah" << endl;
+    }
 }
