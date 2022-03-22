6cbe1d427c453ee68d0d8b886f20371611c4e9a8
LordGedelicious <gede.prasidha@gmail.com>
2022-03-22 09:57:07 +0700

    update crafting, still kinda buggy

--- Crafting.hpp
+++ Crafting.hpp
@@ -14,23 +14,34 @@
 #define _CRAFTING_HPP_
 
 class CraftingTable {
+    // Karena gaada id disini, jadinya gini ya:
+    // C0 -> [0][0], C1 -> [0][1], C2 -> [0][2]
+    // C3 -> [1][0], C4 -> [1][1], C5 -> [1][2]
+    // C6 -> [2][0], C7 -> [2][1], C8 -> [2][2]
     private:
-        tuple<Item*, int> slotTable; // Isinya Item dan jumlah itemnya
+        tuple<Item, int> slotTable; // Isinya Item dan jumlah itemnya
         slotTable *craftingTable[3][3]; // slot table 3x3
     public:
-        // hanya dibuat default constructor. tidak ada user-defined constructor
+        // hanya dibuat default constructor, cctor, dan dtor. tidak ada user-defined constructor
         CraftingTable();
-        // Fungsi getter dan setter
+        CraftingTable(const CraftingTable & other);
+        ~CraftingTable()
+        // Fungsi getter dan setter, row dan column zero-based
         Item getItemByLocation(int row, int column) const;
         int getQtyByLocation(int row, int column) const;
-        void setItemByLocation(int row, int column, Item* item); // by default, jumlahnya 1 dan harus ada di inventory
-        void setItemByLocation(int row, int column, Item* item, int qty); // jumlah qty harus ada di inventory
+        void addItemByLocation(int row, int column, Item newItem, int qty); // jumlah qty harus ada di inventory
         // Fungsi validasi awal isi CraftingTable (basic banget, tapi gw mikir ini bakal ngehemat waktu)
         bool isAllTools();
         bool isAllNonTools();
         // Fungsi mengembalikan array of tuple berisi type item dan posisi semua item di crafting table
         *tuple<string, int> whatIsWhere();
-        Item* craftResults();
+        void craftResults(); // Memanfaatkan returnToInventory
+        // Fungsi menampilkan isi dari CraftingTable
+        void printCraftingTable() const;
+        // Fungsi untuk meletakkan barang di CraftingTable atau Inventory (menggunakan setItemByLocation)
+        void putToCraftingTable(const Item& newItem, int row, int column);
+        void returnToInventory(const Item& newItem, int row, int column);
+        // Diasumsikan tidak butuh fungsi clear
 };
 
 #endif
